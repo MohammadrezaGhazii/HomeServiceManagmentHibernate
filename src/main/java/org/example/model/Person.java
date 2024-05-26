9c2c9495bca5d219f136d015b7f78af58dedef22
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-05-26 12:32:22 +0330

    creat person class

--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -2,8 +2,12 @@ package org.example.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 import org.example.base.entity.BaseEntity;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -14,16 +18,26 @@ import org.example.base.entity.BaseEntity;
 
 @MappedSuperclass
 public class Person extends BaseEntity<Long> {
-    @Column(name = "first_name" , nullable = false)
-    
+    @Column(name = "first_name", nullable = false)
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Name should enter only in letters !!")
     private String firstName;
 
-    @Column(name = "last_name" , nullable = false)
+    @Column(name = "last_name", nullable = false)
+    @Pattern(regexp = "^[A-Za-z\\s]+$", message = "Name should enter only in letters !!")
     private String lastName;
 
-    @Column(name = "national_id", unique = true, nullable = false)
-    private String nationalid;
+    @Column(name = "email", nullable = false, unique = true)
+    @Email(message = "Enter correct type of email , like : ali55@gmail.com")
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    @Pattern(regexp = "^(\\\\+98|0)?9\\\\d{9}$",
+            message = "Enter correct type of Phone number , like : 09121111111 or +989121111111")
+    private String phoneNumber;
 
     @Column(nullable = false)
     private String password;
+
+    @Column(name = "register_date" , nullable = false)
+    private LocalDate registerDate;
 }
