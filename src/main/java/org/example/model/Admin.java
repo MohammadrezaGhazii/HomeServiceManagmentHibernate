78bba230182938728ba2d9f02dfd3100f0cd568c
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-05-26 12:40:30 +0330

    creat admin and client classes

---
+++ src/main/java/org/example/model/Admin.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "admin")
+public class Admin extends Person{
+}
---
+++ src/main/java/org/example/model/Client.java
@@ -0,0 +1,7 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "client")
+public class Client extends Person{
+}
