edac9e4d141e615b631a2aae44053318985502c3
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-05-26 12:43:45 +0330

    add lombok

--- src/main/java/org/example/model/Admin.java
+++ src/main/java/org/example/model/Admin.java
@@ -1,6 +1,14 @@
 package org.example.model;
 
 import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 
 @Entity(name = "admin")
 public class Admin extends Person{
