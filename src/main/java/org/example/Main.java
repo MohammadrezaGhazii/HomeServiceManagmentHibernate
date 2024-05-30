61b1f05401a245cf0d6aa350f1ed63a8ba300842
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-05-30 14:23:27 +0330

    add menu to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.conncetion.SessionFactorySingleton;
+import org.example.menu.MainMenu;
+import org.hibernate.SessionFactory;
+
 public class Main {
     public static void main(String[] args) {
-
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.mainMenu();
     }
 }
