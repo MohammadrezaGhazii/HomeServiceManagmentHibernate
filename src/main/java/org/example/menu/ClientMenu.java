9942dcc670cd33159e750a44d586e086c26725cc
maamarezaa <mohammadrezaghazii19@gmail.com>
2024-06-02 18:13:25 +0330

    add changePassword method

--- src/main/java/org/example/menu/ClientMenu.java
+++ src/main/java/org/example/menu/ClientMenu.java
@@ -1,21 +1,27 @@
 package org.example.menu;
 
+import org.example.model.Client;
+import org.example.service.client.ClientService;
+import org.example.utilities.ApplicationContext;
+import org.example.utilities.Validation;
+
 import java.util.InputMismatchException;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class ClientMenu {
     Scanner scanner = new Scanner(System.in);
+    ClientService clientService = ApplicationContext.getClientService();
 
     public void clientMenu(String email) {
         int numberInput = -1;
         while (numberInput != 0) {
             System.out.println("*** CLIENT MENU ***");
-            System.out.println("1-Add new service");
-            System.out.println("2-Add new sub-service");
-            System.out.println("3-Add new sub-service");
-            System.out.println("4-Add new sub-service");
+            System.out.println("1-Add new order");
+            System.out.println("2-Change your password");
             System.out.println("0-Exit");
             System.out.println();
+
             try {
                 System.out.println("Enter a number:");
                 numberInput = scanner.nextInt();
@@ -28,11 +34,33 @@ public class ClientMenu {
             switch (numberInput) {
                 case 1 -> {
                 }
-                case 2 -> {
-                }
+                case 2 -> changePassword(email);
                 case 0 -> System.out.println("Returned to previous menu");
                 default -> System.out.println("Wrong input");
             }
         }
+    }
+    private void changePassword(String email){
+        Optional<Client> clientOptional = clientService.searchWithEmail(email);
+        if (clientOptional.isPresent()){
+            System.out.println("!!! You Are Changing Your PASSWORD !!!");
+            String password = getValidPassword();
+
+            Client client = clientOptional.get();
+            client.setPassword(password);
+            clientService.saveOrUpdate(client);
+        }
+    }
+    public String getValidPassword() {
+        String password;
+        while (true) {
+            System.out.println("Please enter your password :");
+            password = scanner.nextLine();
+            if (Validation.isPasswordValid(password))
+                break;
+            else
+                System.out.println("This " + password + " is not strong , try again");
+        }
+        return password;
     }
 }
--- src/main/java/org/example/menu/SpecialistMenu.java
+++ src/main/java/org/example/menu/SpecialistMenu.java
@@ -1,18 +1,25 @@
 package org.example.menu;
 
+import org.example.model.Client;
+import org.example.model.Specialist;
+import org.example.service.specialist.SpecialistService;
+import org.example.utilities.ApplicationContext;
+import org.example.utilities.Validation;
+
 import java.util.InputMismatchException;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class SpecialistMenu {
     Scanner scanner = new Scanner(System.in);
-    public void specialistMenu() {
+    SpecialistService specialistService = ApplicationContext.getSpecialistService();
+
+    public void specialistMenu(String email) {
         int numberInput = -1;
         while (numberInput != 0) {
-            System.out.println("*** ADMIN MENU ***");
-            System.out.println("1-Add new service");
-            System.out.println("2-Add new sub-service");
-            System.out.println("3-Add new sub-service");
-            System.out.println("4-Add new sub-service");
+            System.out.println("*** Specialist MENU ***");
+            System.out.println("1-");
+            System.out.println("2-Change your password");
             System.out.println("0-Exit");
             System.out.println();
             try {
@@ -25,11 +32,35 @@ public class SpecialistMenu {
             }
 
             switch (numberInput) {
-                case 1 -> {}
-                case 2 -> {}
+                case 1 -> {
+                }
+                case 2 -> changePassword(email);
                 case 0 -> System.out.println("Returned to previous menu");
                 default -> System.out.println("Wrong input");
             }
         }
+    }
+    private void changePassword(String email){
+        Optional<Specialist> specialistOptional = specialistService.searchWithEmail(email);
+        if (specialistOptional.isPresent()){
+            System.out.println("!!! You Are Changing Your PASSWORD !!!");
+            String password = getValidPassword();
+
+            Specialist specialist = specialistOptional.get();
+            specialist.setPassword(password);
+            specialistService.saveOrUpdate(specialist);
+        }
+    }
+    public String getValidPassword() {
+        String password;
+        while (true) {
+            System.out.println("Please enter your password :");
+            password = scanner.nextLine();
+            if (Validation.isPasswordValid(password))
+                break;
+            else
+                System.out.println("This " + password + " is not strong , try again");
+        }
+        return password;
     }
 }
